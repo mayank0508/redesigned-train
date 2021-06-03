11aa48c3e7ab291662b62afa465a8021bd626598
mayank0508 <thakurmayank0508@gmail.com>
2021-06-03 20:21:30 +0530

    start of the home page 🏠

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
 
 const Home = (props) => {
-    return<div>Home</div>;
-}
+    return (
+    <Container>
+        <Section>
+            <h5>
+                <a>
+                    Hi Hi Hi !
+                </a>
+                </h5>
+                <p>
+                    WELCOME TO THE PLACE WHERE YOU ARE ALLOWED TO HAVE A OPINION !
+                </p>   
+        </Section>
+    </Container>
+    );
+};
+
+const Container = styled.div`
+padding:  52px;
+max-width: 100%;
+`;
+
+const Content = styled.div`
+
+`;
+
+const Section = styled.section`
+
+`;
 
 export default Home;
