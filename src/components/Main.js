30395d44c88d386c30bc8982b2de4f1bfa5dfe90
mayank0508 <thakurmayank0508@gmail.com>
2021-06-07 03:13:08 +0530

    done with the post creation process

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
 import PostModal from './PostModal';
+import { useState } from 'react';
 
 const Main = props => {
+  const [showModal, setShowModal] = useState('close');
+
+  const handleClick = e => {
+    e.preventDefault();
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+
+    switch (showModal) {
+      case 'open':
+        setShowModal('close');
+        break;
+      case 'close':
+        setShowModal('open');
+        break;
+      default:
+        setShowModal('close');
+        break;
+    }
+  };
+
   return (
     <Container>
       <ShareBox>
@@ -11,7 +33,7 @@ const Main = props => {
           ) : (
             <img src="/images/user.svg" alt="" />
           )}
-          <button>Start a post !</button>
+          <button onClick ={handleClick}>Start a post !</button>
         </div>
         <div>
           <button>
@@ -88,7 +110,7 @@ const Main = props => {
           </SocialActions>
         </Article>
       </div>
-      <PostModal />
+      <PostModal showModal ={showModal} handleClick={handleClick} />
     </Container>
   );
 };
@@ -276,4 +298,6 @@ const SocialActions = styled.div`
     }
   }
 `;
+
+
 export default Main;
--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,17 +1,61 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const PostModal = props => {
+  const [editorText, setEditorText] = useState('');
+
+  const reset = (e) => {
+    setEditorText("");
+    props.handleClick(e);
+  };
+
   return (
+    <>
+    { props.showModal === 'open' && 
     <Container>
       <Content>
         <Header>
           <h2>Create a new post</h2>
-          <button>
+          <button onClick={(event) => reset(event)}>
             <span>❌</span>
           </button>
         </Header>
+        <SharedContent>
+          <UserInfo>
+            <img src="/images/user.svg" alt="" />
+            <span>Me</span>
+          </UserInfo>
+          <Editor>
+            <textarea
+              value={editorText}
+              onChange={e => setEditorText(e.target.value)}
+              placeholder="Feeling Dangerous today ?"
+              autoFocus={true}
+            ></textarea>
+          </Editor>
+        </SharedContent>
+        <SharedCreation>
+          <AttachAsset>
+            <AssetButton>
+              <span>📸</span>
+            </AssetButton>
+            <AssetButton>
+              <span>🎥</span>
+            </AssetButton>
+          </AttachAsset>
+          <SharedComment>
+            <AssetButton>
+              <span>💬</span>
+              anyone
+            </AssetButton>
+          </SharedComment>
+
+          <PostButton>Post</PostButton>
+        </SharedCreation>
       </Content>
     </Container>
+}
+    </>
   );
 };
 
@@ -24,6 +68,7 @@ const Container = styled.div`
   z-index: 9999;
   color: black;
   background-color: rgba(0, 0, 0, 0.85);
+  animation: fadeIn 0.3s;
 `;
 
 const Content = styled.div`
@@ -41,23 +86,119 @@ const Content = styled.div`
 `;
 
 const Header = styled.div`
-display: block;
-padding: 16px 2px;
-border-bottom: 3px solid black;
-font-size: 16px;
-line-height: 1.5;
-color: black;
-justify-content: space-between;
-display: flex;
-align-items: center;
-button {
+  display: block;
+  padding: 16px 20px;
+  border-bottom: 3px solid black;
+  font-size: 16px;
+  line-height: 1.5;
+  font-weight: bold;
+  display: flex;
+  color: black;
+  justify-content: space-between;
+  align-items: center;
+  button {
     height: 40px;
     width: 40px;
     min-width: auto;
     color: black;
     border-radius: 120px;
-}
+  }
+  span {
+    pointer-events: none;
+  }
 `;
 
+const SharedContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 24 px;
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50px;
+  }
+  span {
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+  }
+`;
+
+const SharedCreation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+
+const AssetButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  min-width: auto;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const AttachAsset = styled.div`
+  align-items: center;
+  display: flex;
+  padding-right: 8px;
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+
+const SharedComment = styled.div`
+  padding-left: 8px;
+  margin-right: auto;
+  border-left: 1px solid black;
+  ${AssetButton} {
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const PostButton = styled.button`
+  min-width: 60px;
+  font-weight: bold;
+  border-radius: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background-color: white;
+  color: black;
+  &:hover {
+    background-color: #ffee00;
+  }
+`;
+
+const Editor = styled.div`
+  padding: 12px 24px;
+  textarea {
+    width: 100%;
+    min-height: 100px;
+    resize: none;
+  }
+
+  input {
+    width: 100%;
+    height: 35px;
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+`;
 
 export default PostModal;
